0381bf575a043119f199a06868bb9e6b1e438bfb
Clemens JS Schafer <clemens@Clemenss-MacBook-Pro.local>
2020-09-01 22:52:48 -0500

    dvsasl

--- torchneuromorphic/dvs_asl/create_hdf5.py
+++ torchneuromorphic/dvs_asl/create_hdf5.py
@@ -92,23 +92,22 @@ def create_events_hdf5(directory, hdf5_filename):
             #end_tms = labels_starttime[:,2]
             #out = []
 
-            for i, v in enumerate(lbls):
-                if istrain: 
-                    train_keys.append(key)
-                else:
-                    test_keys.append(key)
-                #s_ = get_slice(tms, ads, start_tms[i], end_tms[i])
-                #times = s_[0]
-                #addrs = s_[1]
-                #subj, light = file_d.split('/')[-1].split('.')[0].split('_')[:2]
-                metas.append({'key':str(key), 'training sample':istrain}) # 'subject':subj,'light condition':light,
-                subgrp = data_grp.create_group(str(key))
-                tm_dset = subgrp.create_dataset('times' , data=times, dtype=np.uint32)
-                ad_dset = subgrp.create_dataset('addrs' , data=addrs, dtype=np.uint8)
-                lbl_dset= subgrp.create_dataset('labels', data=lbls[i]-1, dtype=np.uint8)
-                subgrp.attrs['meta_info'] = str(metas[-1])
-                #assert lbls[i]-1 in range(11)
-                key += 1
+            if istrain: 
+                train_keys.append(key)
+            else:
+                test_keys.append(key)
+            #s_ = get_slice(tms, ads, start_tms[i], end_tms[i])
+            #times = s_[0]
+            #addrs = s_[1]
+            #subj, light = file_d.split('/')[-1].split('.')[0].split('_')[:2]
+            metas.append({'key':str(key), 'training sample':istrain}) # 'subject':subj,'light condition':light,
+            subgrp = data_grp.create_group(str(key))
+            tm_dset = subgrp.create_dataset('times' , data=times, dtype=np.uint32)
+            ad_dset = subgrp.create_dataset('addrs' , data=addrs, dtype=np.uint8)
+            lbl_dset= subgrp.create_dataset('labels', data=lbls[i]-1, dtype=np.uint8)
+            subgrp.attrs['meta_info'] = str(metas[-1])
+            #assert lbls[i]-1 in range(11)
+            key += 1
         extra_grp.create_dataset('train_keys', data=train_keys)
         extra_grp.create_dataset('test_keys', data=test_keys)
         extra_grp.attrs['N'] = len(train_keys) + len(test_keys)
